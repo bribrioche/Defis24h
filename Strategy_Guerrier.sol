dfe5dbf014d6b37445d856f68f64cf81c799065b
Yamagi84 <vincent.agueda@outlook.fr>
2021-11-20 22:10:24 +0100

    Strategy_Guerrier

--- Strategy_Guerrier.sol
+++ Strategy_Guerrier.sol
@@ -11,9 +11,31 @@ contract Strategy_Guerrier is ActionStrategy{
         AvatarInterface adversaire = AvatarInterface(avatarAdv);
         AvatarInterface moi = AvatarInterface(avatar);
         
-        if (keccak256(abi.encodePacked("a")) == keccak256(abi.encodePacked("b"))) {
-          return Action.DODGE;
+        if(moi.getVie()<=5 && (previousAdvAction==Action.PHYSICAL_PARRY || previousAdvAction==Action.MAGICAL_PARRY || previousAdvAction==Action.DODGE)) {
+            return Action.DODGE;
         }
-        return Action.NONE;    
+        else {
+            if (keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Guerrier")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Assassin")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Amazone")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Barbare"))) {
+                if (moi.getVie()<=20 && (previousAdvAction==Action.PHYSICAL_PARRY || previousAdvAction==Action.MAGICAL_PARRY || previousAdvAction==Action.DODGE)) {
+                    return Action.PHYSICAL_PARRY;
+                }
+                else {
+                    return Action.PHYSICAL_ATTACK;
+                }
+            }
+            
+            else if (keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Mage")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Archere"))) {
+                if (moi.getVie()<=20 && (previousAdvAction==Action.PHYSICAL_PARRY || previousAdvAction==Action.MAGICAL_PARRY || previousAdvAction==Action.DODGE)) {
+                    return Action.MAGICAL_PARRY;
+                }
+                else {
+                    return Action.PHYSICAL_ATTACK;
+                }
+            }
+            
+            else {
+                return Action.PHYSICAL_ATTACK;
+            }
+        }
     }
 }
