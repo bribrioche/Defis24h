0f3d37111739bc7aa22c0974aad9af8d46e4f04d
Bryan <bryan.guillot@outlook.com>
2021-11-20 22:11:08 +0100

    commit random

---
+++ ContractStrategyMage.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.22 <0.6.0;
+import "ActionStrategy.sol";
+import "AvatarInterface.sol";
+
+contract ContractStrategyWarrior is ActionStrategy{
+    
+    constructor() public {}
+    
+    function chooseNextAction(Action previousAdvAction, address avatar, int damages, address avatarAdv, int damagesAdv) external view returns(Action action){
+       AvatarInterface adversaire = AvatarInterface(avatarAdv);
+        AvatarInterface moi = AvatarInterface(avatar);
+        
+        if((keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Guerrier"))){
+            
+        }
+    }
+    
+}
--- ContractStrategyWarrior.sol
+++ ContractStrategyWarrior.sol
@@ -4,46 +4,31 @@ import "AvatarInterface.sol";
 
 contract ContractStrategyWarrior is ActionStrategy{
     
-<<<<<<< HEAD
-=======
-
->>>>>>> cd34b154d52a957ef45be70fca2301ffdfa790c7
     constructor() public {}
     
     function chooseNextAction(Action previousAdvAction, address avatar, int damages, address avatarAdv, int damagesAdv) external view returns(Action action){
         AvatarInterface adversaire = AvatarInterface(avatarAdv);
         AvatarInterface moi = AvatarInterface(avatar);
-        
-        
-        
-        if(previousAdvAction == Action.PHYSICAL_ATTACK) {
-            if(adversaire.getDefenseMagique() > adversaire.getDefensePhysique()) {
-                return Action.PHYSICAL_PARRY;
-                if(moi.getPuissanceMagique()>moi.getPuissancePhysique()) {
+         
+        if(previousAdvAction == Action.PHYSICAL_ATTACK){      
+            if(adversaire.getDefenseMagique()>adversaire.getDefensePhysique()){
+                if(moi.getPuissanceMagique()>moi.getPuissancePhysique()){
                     return Action.MAGICAL_ATTACK;
-                }
-                else {
+                }else{
                     return Action.PHYSICAL_ATTACK;
                 }
                 
-            }
-            else{
-                return Action.PHYSICAL_PARRY;
-                /*
-                if(moi.getPuissanceMagique()>moi.getPuissancePhysique()) {
+            }else{
+                if(moi.getPuissanceMagique()>moi.getPuissancePhysique()){
                     return Action.MAGICAL_ATTACK;
+                }else{
+                    return Action.PHYSICAL_ATTACK;
                 }
-                else{
-                     return Action.PHYSICAL_ATTACK;
-                }*/
-            }   
-        }
-        else {
+            }      
+         
+    
+        }else{
             return Action.MAGICAL_PARRY;
         }
-    }
-    
-    function testeuh(address perso)external view returns(int value) {
-        return AvatarInterface(perso).getDefensePhysique();
     }
 }
