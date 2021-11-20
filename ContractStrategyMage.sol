1f089e5f2c3e48bc1db08be202e94f158220646c
Bryan <bryan.guillot@outlook.com>
2021-11-20 22:14:19 +0100

    strat mage

--- ContractStrategyMage.sol
+++ ContractStrategyMage.sol
@@ -10,8 +10,31 @@ contract ContractStrategyWarrior is ActionStrategy{
        AvatarInterface adversaire = AvatarInterface(avatarAdv);
         AvatarInterface moi = AvatarInterface(avatar);
         
-        if((keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Guerrier"))){
+        if(moi.getVie()<=5 && (previousAdvAction==Action.PHYSICAL_PARRY || previousAdvAction==Action.MAGICAL_PARRY || previousAdvAction==Action.DODGE)) {
+            return Action.DODGE;
+        }
+        else {
+            if (keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Guerrier")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Assassin")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Amazone")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Barbare"))) {
+                if (moi.getVie()<=20 && (previousAdvAction==Action.PHYSICAL_PARRY || previousAdvAction==Action.MAGICAL_PARRY || previousAdvAction==Action.DODGE)) {
+                    return Action.PHYSICAL_PARRY;
+                }
+                else {
+                    return Action.MAGICAL_ATTACK;
+                }
+            }
             
+            else if (keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Mage")) || keccak256(abi.encodePacked(adversaire.getType())) == keccak256(abi.encodePacked("Archere"))) {
+                if (moi.getVie()<=20 && (previousAdvAction==Action.PHYSICAL_PARRY || previousAdvAction==Action.MAGICAL_PARRY || previousAdvAction==Action.DODGE)) {
+                    return Action.MAGICAL_PARRY;
+                }
+                else {
+                    return Action.MAGICAL_ATTACK;
+                }
+            }
+            
+            else {
+                return Action.MAGICAL_ATTACK;
+            }
         }
     }
     
