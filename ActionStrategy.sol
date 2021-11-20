3e48fdd3eb443036705f5cc0dd5bcb5bd75938d8
Bryan <bryan.guillot@outlook.com>
2021-11-20 17:04:06 +0100

    implements exterieur

--- ActionStrategy.sol
+++ ActionStrategy.sol
@@ -22,27 +22,3 @@ interface ActionStrategy {
      */
     function chooseNextAction(Action previousAdvAction, address avatar, int damages, address avatarAdv, int damagesAdv) external view returns(Action action);
 }
-
-contract Strategy is ActionStrategy {
-    
-    constructor() public {}
-    
-    function chooseNextAction(Action previousAdvAction, address avatar, int damages, address avatarAdv, int damagesAdv) external view returns(Action action){
-        if(previousAdvAction == Action(0)){
-            
-        }
-        address me = avatar;
-        address adv = avatarAdv;
-        if(me != adv){
-            
-        }
-        
-        if(damages<damagesAdv){
-           return Action(1); 
-        }else{
-            return Action(0);
-        }
-        
-    }
-    
-}
