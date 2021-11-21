915489706d4b940824d97ecb05545b47e924f7dc
MaiwennLeCacheux <lecacheux.maiwenn@outlook.fr>
2021-11-21 02:18:23 +0100

    faut coder le code de l exploration la dedans en faiit

---
+++ Explorateur.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+import "WorldInterface.sol";
+
+
+contract Explorateur {
+    
+    WorldInterface world;
+    
+    
+    constructor(){
+
+        //intanciation du world 
+        world = WorldInterface(*adress*);
+    }
+    
+    function explorer(){
+        //getcoulieu
+        //verif si c'est occupé
+        //etc
+        
+        
+        
+        //explorer()
+    }
+    
+    
+    
+    
+}
